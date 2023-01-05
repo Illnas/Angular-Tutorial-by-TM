7eecc10b5145dce34fb41da9a5008ad1e01113d2
Nadatari <dariosting@gmail.com>
2023-01-05 18:57:51 +0100

    Started tutorial on Angular by Traversy Media

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks()
+  }
+
+  tasks: Task[] = [];
+}
